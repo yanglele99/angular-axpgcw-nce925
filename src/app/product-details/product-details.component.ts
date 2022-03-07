2f22772041dd1c41290950b8607dabb9c4e780ee
yanglele99 <dreamer_xll@163.com>
2022-03-07 14:03:18 +0800

    添加导航

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
-  ngOnInit(): void {
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
   }
-
 }
